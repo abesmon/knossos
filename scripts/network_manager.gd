802ab07b3e4e3c3524099d5c0c6f8912648b6911
Alexey Lysenko <abesmon@gmail.com>
2026-06-19 00:23:02 +0300

    дополнительные ice сервера

--- scripts/network_manager.gd
+++ scripts/network_manager.gd
@@ -35,7 +35,35 @@ signal voice_received(id: int, payload: PackedByteArray)
 const MAX_CHAT_CHARS := 280
 
 const ICE_SERVERS := {
-	"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+	"iceServers": [
+		{
+			"urls": ["stun:stun.l.google.com:19302"]
+		},
+		{
+			"urls": "stun:stun.relay.metered.ca:80",
+		},
+		{
+			"urls": "turn:global.relay.metered.ca:80",
+			"username": "609dc0c20c8274554e649868",
+			"credential": "B/2s+iW8VmDd+9AL",
+		},
+		{
+			"urls": "turn:global.relay.metered.ca:80?transport=tcp",
+			"username": "609dc0c20c8274554e649868",
+			"credential": "B/2s+iW8VmDd+9AL",
+		},
+		{
+			"urls": "turn:global.relay.metered.ca:443",
+			"username": "609dc0c20c8274554e649868",
+			"credential": "B/2s+iW8VmDd+9AL",
+		},
+		{
+			"urls": "turns:global.relay.metered.ca:443?transport=tcp",
+			"username": "609dc0c20c8274554e649868",
+			"credential": "B/2s+iW8VmDd+9AL",
+		}
+	]
+	
 }
 
 var _ws: WebSocketPeer
